a567faacdd54e88eacb3b6fb6aa6ba99a0b79dec
João Victor <joaovictortcl@gmail.com>
2024-11-27 08:30:59 -0300

    Update zabbix_script_alert_whatsapp.sh

--- zabbix_script_alert_whatsapp.sh
+++ zabbix_script_alert_whatsapp.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-URL="http://IP_EVOLUTIONAPI/message/sendText/{instance}"
+URL="http://IP_EVOLUTIONAPI/message/sendText/{nome_da_instancia_da_api_evolution}"
 APIKEY="CHAVE DE API INSTACIA EVOLUTION API"
 PHONE="$1"
 TITULO="$2"
 MESSAGE="$TITULO$3"
 
 curl --request POST \
-  --url "${URL}" \
-  --header 'Content-Type: application/json' \
-  --header 'apikey: ${APIKEY}' \
-  --data '{
-  "textMessage": {
-    "text": "${MESSAGE}"
-  },
-  "number": "${PHONE}"
-}'
+  --url "$URL" \
+  --header "Content-Type: application/json" \
+  --header "apikey: $APIKEY" \
+  --data "{
+    \"textMessage\": {
+      \"text\": \"$MESSAGE\"
+    },
+    \"number\": \"$PHONE\"
+  }"
